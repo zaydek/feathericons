86623f898bd9634f16a4360493247abc04bdaedc
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2021-04-17 23:41:26 +0900

    Virtualize prose variables

--- src/index.js
+++ src/index.js
@@ -21,82 +21,58 @@ function App() {
 // Layout
 
 // prettier-ignore
-$layout-map: (
-	flow-margin: rem(32),
-	head-h1-margin: rem(8),
-	head-h2-margin: rem(8),
-	head-h3-margin: rem(8),
-	text-margin: rem(32 / 2),
-	head-h1: (600 rem(32) / 1.4 tw(sans)),
-	head-h2: (600 rem(24) / 1.4 tw(sans)),
-	head-h3: (600 rem(24) / 1.4 tw(sans)),
-	text: (rem(18) / 1.5 tw(sans)),
-	text-small: (rem(14) / 1.8 tw(sans)),
-	code: (em(14) / 1.5 tw(mono)),
-	code-pre: (rem(15) / 1.6 tw(mono)),
-	code-padding: rem(2) rem(4),
-	code-rounded: rem(3),
-	code-pre-padding: rem(16) rem(24),
-	code-pre-rounded: rem(6),
-);
-
-.prose {
-	@each $mk, $mv in $layout-map {
-		--prose-#{$mk}: #{$mv};
-	}
-}
+$prose-flow-margin: rem(32);
+$prose-head-h1-margin: rem(8);
+$prose-head-h2-margin: rem(8);
+$prose-head-h3-margin: rem(8);
+$prose-text-margin: rem(32 / 2);
+$prose-head-h1: (600 rem(32) / 1.4 tw(sans));
+$prose-head-h2: (600 rem(24) / 1.4 tw(sans));
+$prose-head-h3: (600 rem(24) / 1.4 tw(sans));
+$prose-text: (rem(18) / 1.5 tw(sans));
+$prose-text-small: (rem(14) / 1.8 tw(sans));
+$prose-code: (em(14) / 1.5 tw(mono));
+$prose-code-pre: (rem(15) / 1.6 tw(mono));
+$prose-code-padding: rem(2) rem(4);
+$prose-code-rounded: rem(3);
+$prose-code-pre-padding: rem(16) rem(24);
+$prose-code-pre-rounded: rem(6);
 
 // Themes
 
-$light-map: (
-	head-h1-color: tw(black),
-	head-h2-color: tw(black),
-	head-h3-color: tw(black),
-	text-color: tw(cool-gray-800),
-	text-anchor-color: tw(cool-gray-600),
-	text-small-color: tw(cool-gray-700),
-	hr-border: 2px solid tw(cool-gray-200),
-	code-color: hsl(220, 83.33%, 50%),
-	code-bg: tw(white),
-	code-border: 1px solid tw(cool-gray-300),
-	code-pre-color: tw(cool-gray-100),
-	code-pre-bg: tw(cool-gray-800),
-	code-pre-border: none,
-);
+$prose-head-h1-color:     tw(black);
+$prose-head-h2-color:     tw(black);
+$prose-head-h3-color:     tw(black);
+$prose-text-color:        tw(cool-gray-800);
+$prose-text-anchor-color: tw(cool-gray-600);
+$prose-text-small-color:  tw(cool-gray-700);
+$prose-hr-border:         2px solid tw(cool-gray-200);
+$prose-code-color:        hsl(220, 83.33%, 50%);
+$prose-code-bg:           tw(white);
+$prose-code-border:       1px solid tw(cool-gray-300);
+$prose-code-pre-color:    tw(cool-gray-100);
+$prose-code-pre-bg:       tw(cool-gray-800);
+$prose-code-pre-border:   none;
 
 // FIXME: 1px solid transparent
 //
 // https://github.com/zaydek/duomo/issues/203
-$dark-map: (
-	head-h1-color: tw(cool-gray-100),
-	head-h2-color: tw(cool-gray-100),
-	head-h3-color: tw(cool-gray-100),
-	text-color: tw(cool-gray-200),
-	text-anchor-color: tw(cool-gray-200),
-	text-small-color: tw(cool-gray-300),
-	hr-border: 2px solid tw(cool-gray-600),
-	code-color: tw(gray-100),
-	code-bg: tw(cool-gray-700),
-	code-border: 1px solid transparent,
-	code-pre-color: tw(cool-gray-100),
-	code-pre-bg: tw(cool-gray-700),
-	code-pre-border: none,
-);
-
-.prose {
-	@each $mk, $mv in $light-map {
-		--prose-#{$mk}: #{$mv};
-	}
-}
-
-[data-theme="dark"] .prose {
-	@each $mk, $mv in $dark-map {
-		--prose-#{$mk}: #{$mv};
-	}
-}
+$prose-head-h1-color-dark:     tw(cool-gray-100);
+$prose-head-h2-color-dark:     tw(cool-gray-100);
+$prose-head-h3-color-dark:     tw(cool-gray-100);
+$prose-text-color-dark:        tw(cool-gray-200);
+$prose-text-anchor-color-dark: tw(cool-gray-200);
+$prose-text-small-color-dark:  tw(cool-gray-300);
+$prose-hr-border-dark:         2px solid tw(cool-gray-600);
+$prose-code-color-dark:        tw(gray-100);
+$prose-code-bg-dark:           tw(cool-gray-700);
+$prose-code-border-dark:       1px solid transparent;
+$prose-code-pre-color-dark:    tw(cool-gray-100);
+$prose-code-pre-bg-dark:       tw(cool-gray-700);
+$prose-code-pre-border-dark:   none;
 
 .prose > * + * {
-	margin-top: var(--prose-flow-margin);
+	margin-top: $prose-flow-margin;
 }
 
 // <article class="prose">
@@ -106,77 +82,68 @@ $dark-map: (
 // </article>
 //
 .prose > :not(div) {
-	font: var(--prose-text);
-	color: var(--prose-text-color);
-	transition: var(--theme-transition);
+	font: $prose-text;
+	@include color($prose-text-color, $prose-text-color-dark);
 }
 
 .prose h1 {
-	font: var(--prose-head-h1);
-	color: var(--prose-head-h1-color);
-	transition: var(--theme-transition);
+	font: $prose-head-h1;
+	@include color($prose-head-h1-color, $prose-head-h1-color-dark);
 }
 .prose h2 {
-	font: var(--prose-head-h2);
-	color: var(--prose-head-h2-color);
-	transition: var(--theme-transition);
+	font: $prose-head-h2;
+	@include color($prose-head-h2-color, $prose-head-h2-color-dark);
 }
 .prose h3,
 .prose h4,
 .prose h5,
 .prose h6 {
-	font: var(--prose-head-h3);
-	color: var(--prose-head-h3-color);
-	transition: var(--theme-transition);
+	font: $prose-head-h3;
+	@include color($prose-head-h3-color, $prose-head-h3-color-dark);
 }
 .prose h1 + p {
-	margin-top: var(--prose-head-h1-margin);
+	margin-top: $prose-head-h1-margin;
 }
 .prose h2 + p {
-	margin-top: var(--prose-head-h2-margin);
+	margin-top: $prose-head-h2-margin;
 }
 .prose h3 + p,
 .prose h4 + p,
 .prose h5 + p,
 .prose h6 + p {
-	margin-top: var(--prose-head-h3-margin);
+	margin-top: $prose-head-h3-margin;
 }
 .prose p + p {
-	margin-top: var(--prose-text-margin);
+	margin-top: $prose-text-margin;
 }
 .prose a {
-	color: var(--prose-text-anchor-color);
+	@include color($prose-text-anchor-color, $prose-text-anchor-color-dark);
 	text-decoration: underline;
-	transition: var(--theme-transition);
 }
 .prose small {
-	font: var(--prose-text-small);
-	color: var(--prose-text-small-color);
-	transition: var(--theme-transition);
+	font: $prose-text-small;
+	@include color($prose-text-small-color, $prose-text-small-color-dark);
 }
 .prose hr {
-	border: var(--prose-hr-border);
-	transition: var(--theme-transition);
+	border: $prose-hr-border;
 }
 .prose *:not(pre) code {
 	@include unantialiased;
-	padding: var(--prose-code-padding);
-	font: var(--prose-code);
-	color: var(--prose-code-color);
-	background-color: var(--prose-code-bg);
-	border: var(--prose-code-border);
-	border-radius: var(--prose-code-rounded);
-	transition: var(--theme-transition);
+	padding: $prose-code-padding;
+	font: $prose-code;
+	@include color($prose-code-color, $prose-code-color-dark);
+	@include background-color($prose-code-bg, $prose-code-bg-dark);
+	border: $prose-code-border;
+	border-radius: $prose-code-rounded;
 }
 .prose pre {
 	@include unantialiased;
-	padding: var(--prose-code-pre-padding);
-	font: var(--prose-code-pre);
-	color: var(--prose-code-pre-color);
-	background-color: var(--prose-code-pre-bg);
-	border: var(--prose-code-pre-border);
+	padding: $prose-code-pre-padding;
+	font: $prose-code-pre;
+	@include color($prose-code-pre-color, $prose-code-pre-color-dark);
+	@include background-color($prose-code-pre-bg, $prose-code-pre-bg-dark);
+	border: $prose-code-pre-border;
 	border-radius: 0; // Reset
-	transition: var(--theme-transition);
 
 	/*
 	 * Overrides
@@ -192,8 +159,8 @@ $dark-map: (
 	.prose pre {
 		margin-left: 0; // Reset
 		margin-right: 0; // Reset
-		padding: var(--prose-code-pre-padding);
-		border-radius: var(--prose-code-pre-rounded);
+		padding: $prose-code-pre-padding;
+		border-radius: $prose-code-pre-rounded;
 	}
 }
 
